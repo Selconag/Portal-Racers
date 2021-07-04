a9de334b0b834e6372a3338969827c891bef518b
Selconag <ooocrazyselcuk@gmail.com>
2021-07-04 18:44:08 +0300

    Upload Project, Initial Version
    This project is for trying to create a Portal System from the games like Portal,Superliminal and like etc...

---
+++ Assets/Teleport.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform port1;
+    public Transform port2;
+    public GameObject Portal1;
+    public GameObject Portal2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            collision.gameObject.transform.position = port2.transform.position;
+            Debug.Log("Do something else here");
+        }
+    }
+    private void OnTriggerEnter2D1(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            collision.gameObject.transform.position = port2.transform.position;
+            Debug.Log("Do something else here");
+        }
+    }
+    public void Teleportate()
+    {
+        
+    }
+}
